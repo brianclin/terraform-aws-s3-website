79b7b57b44aee8c3f5e59bd965ab57c0e699f89f
Brian Lin <briancclin@gmail.com>
2023-06-30 10:54:52 -0400

    feat: add output cloudfront_distribution_hosted_zone_id and cloudfront_distribution_domain_name

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "cloudfront_distribution_id" {
 output "s3_bucket_id" {
     value = aws_s3_bucket.this.id
 }
+
+output "cloudfront_distribution_domain_name" {
+    value = module.cdn.cloudfront_distribution_domain_name
+}
+
+output "cloudfront_distribution_hosted_zone_id" {
+    value = module.cdn.cloudfront_distribution_hosted_zone_id
+}
