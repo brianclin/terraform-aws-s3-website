34e2bad9f8bbcf68d97c6c56db5a5ffbc5e51ef6
Brian Lin <briancclin@gmail.com>
2023-06-30 20:52:00 -0400

    test record

--- route53.tf
+++ route53.tf
@@ -4,7 +4,7 @@ resource "aws_route53_zone" "primary" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "root" {
   count = var.create_hosted_zone ? 1 : 0
 
   zone_id = aws_route53_zone.primary[0].zone_id
@@ -15,4 +15,13 @@ resource "aws_route53_record" "www" {
     zone_id                = module.cdn.cloudfront_distribution_hosted_zone_id
     evaluate_target_health = true
   }
+}
+
+resource "aws_route53_record" "www" {
+  count = var.create_hosted_zone ? 1 : 0
+
+  zone_id = aws_route53_zone.primary[0].zone_id
+  name    = "www"
+  type    = "CNAME"
+  records = ["brianclin.dev"]
 }
