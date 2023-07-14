b550a3078d6b8edfc72aa75db656673e6aebc6ff
Brian Lin <briancclin@gmail.com>
2023-07-14 04:18:53 -0400

    feat: use wildcard cert

--- certificate.tf
+++ certificate.tf
@@ -3,9 +3,8 @@ locals {
 }
 
 resource "aws_acm_certificate" "cert" {
-  domain_name               = var.domain_name
-  subject_alternative_names = ["www.${var.domain_name}"]
-  validation_method         = "DNS"
+  domain_name       = "*.${var.domain_name}"
+  validation_method = "DNS"
 
   lifecycle {
     create_before_destroy = true
