b10b3819f5884b83127d182aff499561d92e36a1
Brian Lin <briancclin@gmail.com>
2023-07-14 04:22:52 -0400

    fix: add root domain

--- certificate.tf
+++ certificate.tf
@@ -3,8 +3,9 @@ locals {
 }
 
 resource "aws_acm_certificate" "cert" {
-  domain_name       = "*.${var.domain_name}"
-  validation_method = "DNS"
+  domain_name               = "*.${var.domain_name}"
+  subject_alternative_names = [var.domain_name]
+  validation_method         = "DNS"
 
   lifecycle {
     create_before_destroy = true
